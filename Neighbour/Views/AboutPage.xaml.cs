1b515e5172733c1a86385a91053e733b71aec6bf
Umar Ali <umar.ali@mindstormstudios.com>
2017-08-18 00:44:48 +0500

    Do initial commit

---
+++ Neighbour/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Neighbour
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
